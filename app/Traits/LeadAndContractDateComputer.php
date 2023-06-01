fa21cf190589b761306afdff2c1f93d6c602b969
wilsonbacaling33 <wilsonbacaling@gmail.com>
2023-06-01 16:06:14 +0800

    add two fields

--- app/Traits/LeadAndContractDateComputer.php
+++ app/Traits/LeadAndContractDateComputer.php
@@ -9,11 +9,15 @@ trait LeadAndContractDateComputer
         $totalNumberOfLeads = 0;
         $totalNumberOfContracts = 0;
         $grandTotalContracts = 0;
+        $totalConversionRate = 0;
+        $grandTotalAveragePrice = 0;
         
         foreach ($results as $result){
             $totalNumberOfLeads = $totalNumberOfLeads + $result['totalLeads'];
             $totalNumberOfContracts = $totalNumberOfContracts + $result['totalContracts'];
             $grandTotalContracts = $grandTotalContracts + $result['sumOfTotalContracts'];
+            $totalConversionRate = $totalConversionRate + $result['conversionRate'];
+            $grandTotalAveragePrice = $grandTotalAveragePrice + $result['averageSalesPrice'];
         }
         
         $resultLength = count($results);
@@ -22,6 +26,8 @@ trait LeadAndContractDateComputer
             'totalNumberOfLeads' => $totalNumberOfLeads,
             'totalNumberOfContracts' => $totalNumberOfContracts,
             'grandTotalContracts' => $grandTotalContracts,
+            'averageConversionRate' => $totalNumberOfLeads == 0? 0: $totalNumberOfContracts / $totalNumberOfLeads,
+            'grandAveragePrice' => $totalNumberOfContracts == 0? 0: $grandTotalContracts / $totalNumberOfContracts,
         ];
     }
 }

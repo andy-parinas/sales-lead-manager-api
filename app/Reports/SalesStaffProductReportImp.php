1307abac9f41bf3006d31ae26110c96eec8cb8b1
wilsonbacaling <52906225+wilsonbacs@users.noreply.github.com>
2024-08-07 16:33:10 +0800

    Fixes on reports that has different products

--- app/Reports/SalesStaffProductReportImp.php
+++ app/Reports/SalesStaffProductReportImp.php
@@ -9,24 +9,24 @@ class SalesStaffProductReportImp implements Interfaces\SalesStaffProductReport
 {
     public function generate($queryParams)
     {
-        $mainQuery= DB::table('leads')
-            ->selectRaw("concat(sales_staff.first_name, ' ', sales_staff.last_name) as salesStaff")
-            ->selectRaw("products.name as productName")
-            ->selectRaw("GROUP_CONCAT(DISTINCT franchises.franchise_number)  as franchiseNumber")
-            ->selectRaw("avg(contracts.total_contract) as averageSalesPrice")
-            ->selectRaw("count(leads.id) as numberOfLeads")
-            ->selectRaw("count( IF (contracts.contract_price > 0 and appointments.outcome = 'success' , 1, null)) as numberOfSales")
-            ->selectRaw("count( IF (contracts.contract_price > 0 and appointments.outcome = 'success' , 1, null)) / count(leads.id) * 100 as conversionRate")
-            ->selectRaw("sum(contracts.total_contract) as totalContracts")
-            ->join("job_types", "job_types.lead_id", "=", "leads.id")
-            ->join("products", "job_types.product_id", "=", "products.id")
-            ->join("appointments", "appointments.lead_id", "=", "leads.id")
-            ->join("sales_staff", "job_types.sales_staff_id", '=', "sales_staff.id")
-            ->join("franchises", "leads.franchise_id", "=", "franchises.id")
-            ->rightJoin("contracts", "contracts.lead_id", "=", "leads.id");
-
+        $mainQuery = DB::table('leads')
+        ->selectRaw("concat(sales_staff.first_name, ' ', sales_staff.last_name) as salesStaff")
+        ->selectRaw("GROUP_CONCAT(DISTINCT products.name) as productName")
+        ->selectRaw("GROUP_CONCAT(DISTINCT franchises.franchise_number) as franchiseNumber")
+        ->selectRaw("avg(contracts.total_contract) as averageSalesPrice")
+        ->selectRaw("count(leads.id) as numberOfLeads")
+        ->selectRaw("count( IF (contracts.contract_price > 0 and appointments.outcome = 'success' , 1, null)) as numberOfSales")
+        ->selectRaw("count( IF (contracts.contract_price > 0 and appointments.outcome = 'success' , 1, null)) / count(leads.id) * 100 as conversionRate")
+        ->selectRaw("sum(contracts.total_contract) as totalContracts")
+        ->join("job_types", "job_types.lead_id", "=", "leads.id")
+        ->join("products", "job_types.product_id", "=", "products.id")
+        ->join("appointments", "appointments.lead_id", "=", "leads.id")
+        ->join("sales_staff", "job_types.sales_staff_id", '=', "sales_staff.id")
+        ->join("franchises", "leads.franchise_id", "=", "franchises.id")
+        ->rightJoin("contracts", "contracts.lead_id", "=", "leads.id");
+        
         $mainQuery = $this->getQuery($mainQuery, $queryParams);
-
+        
         return $mainQuery->get();
     }
     
